94297f4e875adf051793d81d13523362fd72ce24
Fawaz Haroun <harounayomikun@gmail.com>
2021-06-22 23:43:30 +0100

    fix

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -23,7 +23,6 @@ export default {
   },
   data() {
     return {
-        apiKey: "AIzaSyDV4CrnBqdxolhzpp0aMMRSnA-CXfMiHZo",
         searchValue: "",
         videos: [],
         videoPlaying: false
